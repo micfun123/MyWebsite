94575ad51d28fe59f6632f676fecf0fc6d03074c
micfun123 <54761507+micfun123@users.noreply.github.com>
2024-05-31 14:06:34 +0100

    nicer typer

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     const typewriterElement = document.getElementById('typewriter-heading');
-    const text = 'Welcome to Tea Drinker\'s World';
+    const text = 'Welcome to Tea Drinker\'s World ';
     typewriterElement.innerHTML = `<span class="typewriter">${text}</span>`;
 });
